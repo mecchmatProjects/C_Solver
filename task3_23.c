5156fe2bfa5486c77d1411f4f234e6e8c70ca77e
Mariia Khoroshun <154595892+mariiakhoroshun@users.noreply.github.com>
2026-09-18 12:20:09 +0300

    Add files via upload
    3.23 Пункт ж)

---
+++ task3_23.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool is_zero(double x) {  //Щоб коректно працював is_zero(вже є в Activaton.c)
+    return fabs(x) < 1e-9;
+}
+
+// --- Пункт ж) sinc ---
+
+double sinc(double x) {  //Функція sinc
+    if (is_zero(x)) {
+        return 1.0;
+    } else {
+        return sin(x) / x;
+    }
+}
+
+double sinc_derivative(double x) {  //Похідна функції sinc
+    if (is_zero(x)) {
+        return 0.0;
+    } else {
+        return (x * cos(x) - sin(x)) / (x * x);
+    }
+}
+
+int test_sinc() { //Тест для функції sinc
+    if (!is_zero(sinc(0) - 1.0)) {
+        printf("Test sinc for 0 is failed\n");
+        return 1;
+    }
+    if (!is_zero(sinc(1.0) - sin(1.0))) {
+        printf("Test sinc for 1 is failed\n");
+        return 1;
+    }
+    return 0;
+}
+
+int main() { //Щоб запустити тест
+    if (test_sinc() == 0) {
+        printf("Test passed successfully.\n");
+    } 
+    else {
+        printf("Test failed.\n");
+    }
+}
